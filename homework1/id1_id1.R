d21309d095e3c0181337dcd0e3473aac464e6d2a
David Raviv <david@raviv.co>
2025-11-17 14:57:52 +0200

    question 1

---
+++ homework1/id1_id1.R
@@ -0,0 +1,17 @@
+# (255/0)*9
+# The above will yield nothing as this line is commented out.
+
+
+# round
+# round rounds the values in its first argument to the specified number of decimal places (default 0).
+# round(x, digits = 0)
+#   x	      a numeric vector. Or, for round and signif, a complex vector.
+#   digits	integer indicating the number of decimal places (round) or significant digits (signif) to be used. For round, negative values are allowed (see ‘Details’).
+
+# abs
+# abs(x) computes the absolute value of x.
+
+# sqrt
+# sqrt(x) computes the (principal) square root of x.
+
+round(pi,5)
